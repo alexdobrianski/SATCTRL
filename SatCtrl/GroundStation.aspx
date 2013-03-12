f842980ba2ff2529bf488abd3222b23f5b5b5158
alex dobrianski <adobri@shaw.ca>
2013-03-12 03:28:16 -0700

    positions of TBD ground stations + draft of page
    positions of TBD ground stations + draft of the Ground Station page

--- SatCtrl/GroundStation.aspx
+++ SatCtrl/GroundStation.aspx
@@ -5,26 +5,46 @@
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
     <p>
-        Ground station location XXX, YYY</p>
+        &nbsp;Ground station <%=LatitudeName%>, location <%=Latitude%>, <%=Longitude%> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        Sky view from Ground Station Camera. </p>
     <p>
         <iframe id="I1" frameborder="0" height="350" marginheight="0" marginwidth="0" 
             name="I1" scrolling="no" 
-            src="https://maps.google.ca/?ie=UTF8&amp;ll=49.257735,-123.123904&amp;spn=0.153041,0.316887&amp;t=m&amp;z=12&amp;output=embed" 
+            src="https://maps.google.ca/?ie=UTF8&amp;ll=<%=Latitude%>,<%=Longitude%>&amp;spn=0.153041,0.316887&amp;t=m&amp;z=12&amp;output=embed" 
             width="425"></iframe>
 &nbsp;
+        <asp:Image ID="Image1" runat="server" Height="350px" 
+            ImageUrl="~/Img/SDC10316.JPG" />
         <br />
         <small>
-        <a href="https://maps.google.ca/?ie=UTF8&amp;ll=49.257735,-123.123904&amp;spn=0.153041,0.316887&amp;t=m&amp;z=12&amp;source=embed" 
+        <a href="https://maps.google.ca/?ie=UTF8&amp;ll=<%=Latitude%>,<%=Longitude%>&amp;spn=0.153041,0.316887&amp;t=m&amp;z=12&amp;source=embed" 
             style="color:#0000FF;text-align:left">View Larger Map</a></small></p>
+    <p>
+        Antenna position Azimuth=???, Altitude=???&nbsp; URL address:&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server" Width="215px">http://192.168.0.1:6897/</asp:TextBox>
+&nbsp;(make port 6897 visible from internet)</p>
+    <p>
+        <asp:CheckBox ID="CheckBox1" runat="server" 
+            oncheckedchanged="CheckBox1_CheckedChanged" Text="Enable Main Communication." />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Text="Call Cubesat" />
+    </p>
+    <p>
+        Uplink&nbsp;&nbsp;&nbsp; : 
+        <asp:TextBox ID="TextBox1" runat="server" Height="57px" Width="810px"></asp:TextBox>
+    </p>
+    <p>
+        Downlink:<asp:TextBox ID="TextBox2" runat="server" Height="58px" Width="822px"></asp:TextBox>
+    </p>
     <p>
         Weather at Ground station location
         <span style="display: block !important; width: 320px; text-align: center; font-family: sans-serif; font-size: 12px;">
-        <a href="http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.71892&amp;bannertypeclick=wu_clean2day" 
-            target="_blank" title="Vancouver, British Columbia Weather Forecast">
-        <img alt="Find more about Weather in Vancouver, CA" 
-            src="http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&amp;pwscode=IBRITSHC3&amp;ForcedCity=Vancouver&amp;ForcedState=Canada&amp;wmo=71892&amp;language=EN" 
+        <a href="<%=Weather%>" 
+            target="_blank" title="<%=WeatherTitle%>"">
+        <img alt="<%=WeatherTitle2%>" 
+            src="<%=WeatherTitlepwscode%>"
             width="300" /></a><br />
-        <a href="http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.71892&amp;bannertypeclick=wu_clean2day" 
+        <a href="<%=Weather%>"
             style="font-family: sans-serif; font-size: 12px" target="_blank" 
             title="Get latest Weather Forecast updates">Click for weather forecast</a></span><br />
     </p>
--- SatCtrl/GroundStation.aspx.cs
+++ SatCtrl/GroundStation.aspx.cs
@@ -9,7 +9,118 @@ namespace SatCtrl
 {
     public partial class _GroundStation : System.Web.UI.Page
     {
+        public string LatitudeName;
+        public string Latitude;
+        public string Longitude;
+        public string Weather;
+        public string WeatherTitle;
+        public string WeatherTitle2;
+        public string WeatherTitlepwscode;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            Latitude = "49.257735";
+            Longitude = "-123.123904";
+            Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.71892&amp;bannertypeclick=wu_clean2day";
+            WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&amp;pwscode=IBRITSHC3&amp;ForcedCity=Vancouver&amp;ForcedState=Canada&amp;wmo=71892&amp;language=EN";
+            WeatherTitle = "Vancouver, British Columbia Weather Forecast";
+            WeatherTitle2 = "Find more about Weather in Vancouver, CA";
+            LatitudeName = "Vancouver";
+            String Location = Page.Request.QueryString["st"];
+            if (Location == "1")
+            {
+                Latitude = "49.257735";
+                Longitude = "-123.123904";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.71892&amp;bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&amp;pwscode=IBRITSHC3&amp;ForcedCity=Vancouver&amp;ForcedState=Canada&amp;wmo=71892&amp;language=EN";
+                WeatherTitle = "Vancouver, British Columbia Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Vancouver, CA";
+                LatitudeName = "Vancouver, BC";
+            }
+            if (Location == "2")
+            {
+                Latitude = "27.306121";
+                Longitude = "-82.569112";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:34230.1.99999&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=KSRQ&ForcedCity=Sarasota&ForcedState=FL&zip=34230&language=EN";
+                WeatherTitle = "Sarasota, Florida Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Sarasota, FL";
+                LatitudeName = "Sarasota, Florida (TBD)";
+            }
+            if (Location == "3")
+            {
+                Latitude = "-7.153526";
+                Longitude = "-34.88966";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.82798&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=SBJP&ForcedCity=Joao Pessoa&ForcedState=Brazil&wmo=82798&language=EN";
+                WeatherTitle = "Joao Pessoa, Brazil Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Joao Pessoa, BZ";
+                LatitudeName = "Joao Pessoa, East Brasilia (TBD)";
+            }
+            if (Location == "4")
+            {
+                Latitude = "-33.916743";
+                Longitude = "18.402658";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.68816&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=FACT&ForcedCity=Cape Town&ForcedState=South Africa&wmo=68816&language=EN";
+                WeatherTitle = "Cape Town, South Africa Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Cape Town, ZA";
+                LatitudeName = " Cape Town, South Africa(TBD)";
+            }
+            if (Location == "5")
+            {
+                Latitude = "47.971091";
+                Longitude = "37.712352";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.WUKCC&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=UKCC&ForcedCity=Donetsk&ForcedState=Ukraine&zip=00000&language=EN";
+                WeatherTitle = "Donetsk, Ukraine Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Donetsk, UR";
+                LatitudeName = "Donetsk, Ukraine (TBD)";
+            }
+            if (Location == "6")
+            {
+                Latitude = "49.789355";
+                Longitude = "73.070583";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.WUAKK&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=UAKK&ForcedCity=Karaganda&ForcedState=Kazakhstan&zip=00000&language=EN";
+                WeatherTitle = "Karaganda, Kazakhstan Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Karaganda, KZ";
+                LatitudeName = "Karaganda, Kazakhstan (TBD)";
+            }
+            if (Location == "7")
+            {
+                Latitude = "-31.967891";
+                Longitude = "115.883274";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.94610&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=YPPH&ForcedCity=Perth&ForcedState=Australia&wmo=94610&language=EN";
+                WeatherTitle = "Perth, Western Australia Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Perth, AU";
+                LatitudeName = "Perth, Western Australia (TBD)";
+            }
+            if (Location == "8")
+            {
+                Latitude = "19.707243";
+                Longitude = "-155.064983";
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:96720.1.99999&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=PHTO&ForcedCity=Hilo&ForcedState=HI&zip=96720&language=EN";
+                WeatherTitle = "Hilo, Hawaii Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Hilo, HI";
+                LatitudeName = "Hilo, Hawaii (TBD)";
+            }
+            if (Location == "9")
+            {
+                Latitude = "-21.255302";
+                Longitude = "-159.768333";
+
+                Weather = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.91844&bannertypeclick=wu_clean2day";
+                WeatherTitlepwscode = "http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_clean2day_metric_cond&airportcode=NCRG&ForcedCity=Rarotonga Aws&ForcedState=Cook Islands&wmo=91844&language=EN";
+                WeatherTitle = "Rarotonga Aws, Cook Islands Weather Forecast";
+                WeatherTitle2 = "Find more about Weather in Rarotonga Aws, KU";
+                LatitudeName = "Rarotonga Aws, Cook Islands (TBD)";
+            }
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
