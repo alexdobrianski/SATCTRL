d2723dacb6ae7f9f1c3e477f71b295fbf5b4c564
alex dobrianski <adobri@shaw.ca>
2013-03-21 03:25:50 -0700

    send GET message from Cubesat page

--- SatCtrl/CraftCubeSat.aspx
+++ SatCtrl/CraftCubeSat.aspx
@@ -1,9 +1,11 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
     CodeBehind="CraftCubeSat.aspx.cs" Inherits="SatCtrl._CraftCubeSat" %>
 
-<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
-</asp:Content>
+
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
-    <input id="UpLoadMsg" type="text" /><input id="SendToCubeSat" type="submit" 
-        value="SendToCubeSat" />
-    </asp:Content>
+    <input id="UpLoadMsg" type="text" /><asp:Button ID="Button1" runat="server" 
+    onclick="Button1_Click" Text="Button" />
+&nbsp; 
+    
+
+</asp:Content>
--- SatCtrl/CraftCubeSat.aspx.cs
+++ SatCtrl/CraftCubeSat.aspx.cs
@@ -14,30 +14,47 @@ namespace SatCtrl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //WebRequest request;
-            //Stream dataStream;
-            //String url = "http://www.adobri.com";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebRequest request;
+            Stream dataStream;
+            String url = "http://www.adobri.com";
             //request = WebRequest.Create("http://localhost:3159/Post.aspx?session_no=000000003&packet_type=2&packet_no=00000&d_time=03/15/13 07:00:55.734&g_station=3&gs_time=03/15/13 07:00:55.734&package=qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
-            //request = WebRequest.Create(url);
-            //request.Method = "GET";
-            //string postData = "";
+            //Uri uri = Uri("localhost");//http://localhost:6921/cmd?session_no=000000003&packet_type=2&packet_no=00000&d_time=03/15/1307:00:55.734&g_station=3&gs_time=03/15/1307:00:55.734&package=qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
+            //request = HttpWebRequest.Create("http://localhost:6921/cmd?session_no=000000003&packet_type=2&packet_no=00000&d_time=03/15/1307:00:55.734&g_station=3&gs_time=03/15/1307:00:55.734&package=qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+            request = WebRequest.Create(url);
+            request.Method = "GET";
+            //string postData = "cmd?session_no=000000003&packet_type=2&packet_no=00000&d_time=03/15/1307:00:55.734&g_station=3&gs_time=03/15/1307:00:55.734&package=qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
             //byte[] byteArray = Encoding.UTF8.GetBytes(postData);
 
             // Set the ContentType property of the WebRequest.
             //request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentType = "text/html";
 
             // Set the ContentLength property of the WebRequest.
             //request.ContentLength = byteArray.Length;
 
             // Get the request stream.
-            
+
             //dataStream = request.GetRequestStream();
 
             // Write the data to the request stream.
             //dataStream.Write(byteArray, 0, byteArray.Length);
 
+
+
             // Close the Stream object.
             //dataStream.Close();
+            WebResponse resp = request.GetResponse();
+            long sizeresp = resp.ContentLength;
+            dataStream = resp.GetResponseStream();
+            byte[] byteArray = new byte[sizeresp];
+
+            dataStream.Read(byteArray, 0, (int)sizeresp);
+            dataStream.Close();
+
         }
     }
 }
